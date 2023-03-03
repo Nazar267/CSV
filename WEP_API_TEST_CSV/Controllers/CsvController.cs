5758a64531ff9f35d0ff2de8136f8861d901b466
Nazar <nazarshuhovscky2@gmail.com>
2023-03-03 09:48:11 +0200

    FCommit

---
+++ WEP_API_TEST_CSV/Controllers/CsvController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEP_API_TEST_CSV.models;
+
+namespace WEP_API_TEST_CSV.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CsvController : ControllerBase
+    {
+        private readonly CsvDbContext dbContext;
+
+        public CsvController(CsvDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CSVDoc>>> Get()
+        {
+            return await dbContext.CsvDocs.ToListAsync();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<CSVDoc>> Post(CSVDoc CsvDoc)
+        {
+
+            if (CsvDoc == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                dbContext.CsvDocs.Add(CsvDoc);
+              await dbContext.SaveChangesAsync();
+                return Ok(CsvDoc);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<CSVDoc>> Put(CSVDoc CsvDoc)
+        {
+            if (CsvDoc == null)
+            {
+                return BadRequest();
+            }
+            if (!dbContext.CsvDocs.Any(x => x.Id == CsvDoc.Id))
+            {
+                return NotFound();
+            }
+
+            dbContext.Update(CsvDoc);
+            await dbContext.SaveChangesAsync();
+            return Ok(CsvDoc);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<CSVDoc>> Delete(int id)
+        {
+            CSVDoc CsvDoc = dbContext.CsvDocs.FirstOrDefault(x => x.Id == id);
+            if (CsvDoc == null)
+            {
+                return NotFound();
+            }
+            dbContext.CsvDocs.Remove(CsvDoc);
+            await dbContext.SaveChangesAsync();
+            return Ok(CsvDoc);
+        }
+
+    }
+}
---
+++ WEP_API_TEST_CSV/models/CsvDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WEP_API_TEST_CSV.models
+{
+    public class CsvDbContext:DbContext
+    {
+        public CsvDbContext(DbContextOptions<CsvDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<CSVDoc> CsvDocs { get; set; }
+    }
+}
